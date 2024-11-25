be4e3f0dd8a4032402c62bf5ff0b5fc570adfd0b
unknown <fernandaminga42@gmail.com>
2024-11-25 15:34:36 -0500

    actualizado

--- src/main/java/com/mycompany/espochbiblioteca/EspochBiblioteca.java
+++ src/main/java/com/mycompany/espochbiblioteca/EspochBiblioteca.java
@@ -11,6 +11,6 @@ package com.mycompany.espochbiblioteca;
 public class EspochBiblioteca {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Bienvenidos al sistema de biblioteca");
     }
 }
