765be74782ad6b8b39cf4cb77b7dbe729c18044a
unknown <fernandaminga42@gmail.com>
2024-11-25 15:32:37 -0500

    Mi proyecto

---
+++ src/main/java/com/mycompany/espochbiblioteca/EspochBiblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.espochbiblioteca;
+
+/**
+ *
+ * @author Lizeth
+ */
+public class EspochBiblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
